18282eeecfd5d56b4e7700bdc62be537d08912b7
Derrick Maina <mburumaina648@gmail.com>
2023-04-12 10:59:49 +0300

    displayed the tv shows

--- js/script.js
+++ js/script.js
@@ -8,11 +8,15 @@ const global = {
 //get data 
 async function fetchApiData(endpoint){
 
-   const API_KEY = '';
+   const API_KEY = '226ef6cc048354b80d241b56c066843e';
    const Api_URL = 'https://api.themoviedb.org/3/';
+
+   showSpinner();
 
    const res = await fetch(`${Api_URL}${endpoint}?api_key=${API_KEY}&language=en-US`);
    const data = res.json();
+
+   hideSpinner();
 
    return data;
 }
@@ -64,6 +68,51 @@ async function displayPopularMovies (){
 
 // displayPopularMovies();
 
+
+async function displayPopularShows (){
+   const {results} = await fetchApiData('/tv/top_rated');
+   // console.log(results);
+
+   results.forEach((shows)=>{
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `
+   
+          <a href="tv-details.html?id=${shows.id}">
+           ${
+            shows.poster_path 
+            ?
+            `
+             <img
+              src="https://image.tmdb.org/t/p/w400${shows.poster_path}"
+              class="card-img-top"
+              alt="Show Title"
+            />
+            
+            `:
+            `
+             
+             <img
+              src="images/no-image.jpg"
+              class="card-img-top"
+              alt="${shows.title}"
+            `
+           }
+          </a>
+          <div class="card-body">
+            <h5 class="card-title">${shows.name}</h5>
+            <p class="card-text">
+              <small class="text-muted"> Aired: ${shows.first_air_date}</small>
+            </p>
+          </div>
+          `
+
+          document.querySelector('#popular-shows').appendChild(div);
+
+   });
+   
+}
+
 const init = ()=>{
    switch(global.currentPage) {
       case '/':
@@ -75,7 +124,7 @@ const init = ()=>{
       console.log('movie-details');
       break;
       case '/shows.html':
-         console.log('shows');
+         displayPopularShows();
       break;
       case '/tv-details.html':
          console.log('tv details');
@@ -104,5 +153,15 @@ const highlightLink = ()=>{
   
 }
 
+
+//spinner 
+
+function showSpinner (){
+   document.querySelector('.spinner').classList.add('show')
+}
+
+function hideSpinner (){
+   document.querySelector('.spinner').classList.remove('show')
+}
 
 document.addEventListener('DOMContentLoaded', init);
