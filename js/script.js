bab2b26b33e1476238b0966ca4d8d34a8f9c65ae
Derrick Maina <mburumaina648@gmail.com>
2023-04-13 09:40:04 +0300

    displayed both movies and tv show details

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ const global = {
 //get data 
 async function fetchApiData(endpoint){
 
-   const API_KEY = '';
+   const API_KEY = '226ef6cc048354b80d241b56c066843e';
    const Api_URL = 'https://api.themoviedb.org/3/';
 
    showSpinner();
@@ -68,6 +68,73 @@ async function displayPopularMovies (){
 
 // displayPopularMovies();
 
+// display the movie details 
+
+async function getMovieDetails (){
+
+   const movieId = window.location.search.split('=')[1]
+   // console.log(movieId);
+   const movie = await fetchApiData(`movie/${movieId}`);
+   // console.log(movie);
+
+   const div = document.createElement('div');
+   div.innerHTML=`
+   <div class="details-top">
+          <div>
+           ${
+
+            movie.poster_path
+            ?
+            ` <img
+              src="https://image.tmdb.org/t/p/w400${movie.poster_path}"
+              class="card-img-top"
+              alt="${movie.title}"
+            />`: `
+                <a href="movie-details.html?id=${movie.id}">
+            <img
+              src="images/no-image.jpg"
+              class="card-img-top"
+              alt="${movie.title}"
+            />
+          </a>
+            `
+           }
+          </div>
+          <div>
+            <h2>${movie.title}</h2>
+            <p>
+              <i class="fas fa-star text-primary"></i>
+              ${movie.vote_average.toFixed(1)} / 10
+            </p>
+            <p class="text-muted">Release Date:${movie.release_date}</p>
+            <p>
+              ${movie.overview}
+            </p>
+            <h5>Genres</h5>
+            <ul class="list-group">
+            ${movie.genres.map((genre)=> `<li>${genre.name}</li>`).join('')}
+            </ul>
+            <a href="${movie.homepage}" target="_blank" class="btn">Visit Movie Homepage</a>
+          </div>
+        </div>
+        <div class="details-bottom">
+          <h2>Movie Info</h2>
+          <ul>
+            <li><span class="text-secondary">Budget:</span> ${addcommastoNumbers(movie.budget)}</li>
+            <li><span class="text-secondary">Revenue:</span> ${addcommastoNumbers(movie.revenue)}</li>
+            <li><span class="text-secondary">Runtime:</span> ${movie.runtime} minutes</li>
+            <li><span class="text-secondary">Status:</span> ${movie.status}</li>
+          </ul>
+          <h4>Production Companies</h4>
+          <div class="list-group">${movie.production_companies.map((company)=>` <span>${company.name}</span>`).join('')}</div>
+        </div>
+   `
+   document.querySelector('#movie-details').appendChild(div)
+}
+
+
+
+
 
 async function displayPopularShows (){
    const {results} = await fetchApiData('/tv/top_rated');
@@ -113,6 +180,74 @@ async function displayPopularShows (){
    
 }
 
+
+async function getShowDetails (){
+
+  const showID = window.location.search.split('=')[1]
+  // console.log(showID);
+  const show = await fetchApiData(`tv/${showID}`);
+
+  const div = document.createElement('div');
+  div.innerHTML = `
+  
+   <div class="details-top">
+          <div>
+          ${
+
+            show.poster_path
+            ?
+            ` <img
+              src="https://image.tmdb.org/t/p/w400${show.poster_path}"
+              class="card-img-top"
+              alt="${show.title}"
+            />`: `
+                <a href="movie-details.html?id=${show.id}">
+            <img
+              src="images/no-image.jpg"
+              class="card-img-top"
+              alt="${show.name}"
+            />
+          </a>
+            `
+           }
+           
+          </div>
+          <div>
+            <h2>${show.name}</h2>
+            <p>
+              <i class="fas fa-star text-primary"></i>
+              ${show.vote_average.toFixed(1)} / 10
+            </p>
+            <p class="text-muted">Release Date:${show.air_date}</p>
+            <p>
+              ${show.overview}
+            </p>
+            <h5>Genres</h5>
+            <ul class="list-group">
+             ${show.genres.map((genre)=> ` <li>${genre.name}</li>`).join('')}
+            </ul>
+            <a href="${show.homepage}" target="_blank" class="btn">Visit Show Homepage</a>
+          </div>
+        </div>
+        <div class="details-bottom">
+          <h2>Show Info</h2>
+          <ul>
+            <li><span class="text-secondary">Number Of Episodes:</span> ${show.number_of_episodes}</li>
+            <li>
+              <span class="text-secondary">Last Episode To Air:</span> ${show.last_episode_to_air}
+            </li>
+            <li><span class="text-secondary">Status:</span>${show.status}</li>
+          </ul>
+          <h4>Production Companies</h4>
+          <div class="list-group">${show.production_companies.map((company)=>`<span>${company.name}</span>`).join('')}</div>
+        </div>
+  
+  `
+
+  document.querySelector('#show-details').appendChild(div)
+
+}
+
 const init = ()=>{
    switch(global.currentPage) {
       case '/':
@@ -121,13 +256,13 @@ const init = ()=>{
       break;
 
       case '/movie-details.html':
-      console.log('movie-details');
+      getMovieDetails();
       break;
       case '/shows.html':
          displayPopularShows();
       break;
       case '/tv-details.html':
-         console.log('tv details');
+         getShowDetails();
       break;
       case '/search.html':
          console.log('search');
@@ -163,5 +298,16 @@ function showSpinner (){
 function hideSpinner (){
    document.querySelector('.spinner').classList.remove('show')
 }
+
+
+function addcommastoNumbers(x){
+   x = x.toString();
+    var pattern = /(-?\d+)(\d{3})/;
+    while (pattern.test(x))
+        x = x.replace(pattern, "$1,$2");
+    return x;
+}
+
+// console.log(addcommastoNumbers(300000000000000000));
 
 document.addEventListener('DOMContentLoaded', init);
